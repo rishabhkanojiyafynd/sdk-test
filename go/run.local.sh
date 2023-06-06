cc59e72b9277cd973048f43772a7b4ea14a8b5d5
rishabhkanojiya <rishabhkanojiya@gofynd.com>
2023-06-06 17:05:20 +0530

    setting default template for package

--- go/run.local.sh
+++ go/run.local.sh
@@ -1,4 +1,4 @@
 rm -rf go.mod
 go mod init go_lang_test
-go get -u "github.com/pixelbin-dev/pixelbin-go/v2@PIXB-2491"
+go get -u "github.com/pixelbin-dev/pixelbin-go/v2@<branch-name>"
 go run index.go
